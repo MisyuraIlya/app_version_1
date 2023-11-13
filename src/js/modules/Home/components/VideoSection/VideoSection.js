4f85f64e1daf3598ee97b0a3f527b51fe812e663
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2023-11-13 10:31:31 +0200

    add

--- src/js/modules/Home/components/VideoSection/VideoSection.js
+++ src/js/modules/Home/components/VideoSection/VideoSection.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 import GalaxyVideo from '../../../../components/routs/GalaxyVideo';
+import { useAuth } from '../../../Auth/providers/AuthProvider';
 const VideoSection = () => {
+    const {user} = useAuth()
     return (
     <section id="page1-my" className="section entry-section">
         <GalaxyVideo /> 
         <div className={true? "showcase animated fadeInDown" : "showcase"}>
             <h1 className="h1-2">{'חליטות תה מיוחדות ומוצרי ומארזי חליטות תה מהודרים לקהל הפרטי והעסקי'}</h1>
+            {user &&
             <NavLink to={'/client/catalog/1/0/0?page=1'}>
                 <div className="button-cls">
                     <p>{'לקטלוג המלא'}</p>
                 </div>
             </NavLink>
+            }
+
         </div>
     </section>
     );
--- src/js/modules/Home/pages/HomePage.js
+++ src/js/modules/Home/pages/HomePage.js
@@ -5,17 +5,22 @@ import VideoSection from '../components/VideoSection/VideoSection';
 import SliderSection from '../components/SliderSection/SliderSection';
 import useCategories from '../../Catalog/store/CategoriesStore';
 import ContactFooter from '../../../SharedComponents/ContactFooter';
+import { useAuth } from '../../Auth/providers/AuthProvider';
 const HomePage = () => {
     const {categoriesLvl1} = useCategories()
+    const {user} = useAuth()
     return (
         <div className="home-page">
             <VideoSection/>
-            <SliderSection
-                title={'קטגוריות'} 
-                array={categoriesLvl1}
-                toShow={5} 
-                column ={1}
-            />
+            {user &&
+                        <SliderSection
+                        title={'קטגוריות'} 
+                        array={categoriesLvl1}
+                        toShow={5} 
+                        column ={1}
+                    />
+            }
+
             <ContactFooter/>
         </div>
     );
